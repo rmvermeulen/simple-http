1898c30e1f8d1f6d27f61f50a65946b2e8d17ee1
Rasmus Vermeulen <rasmus_vermeulen@tutanota.com>
2023-04-20 15:05:32 +0200

    read http request

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,23 @@
-use std::net::TcpListener;
+use std::{
+    io::{prelude::*, BufReader},
+    net::{TcpListener, TcpStream},
+};
 
 fn main() {
     let listener = TcpListener::bind("localhost:8080").expect("Failed to bind address");
     println!("Starting the server...");
     for stream in listener.incoming() {
         let stream = stream.expect("Failed to get stream");
-        println!("Connection established! {:?}", stream.peer_addr());
-        stream.wri
+
+        handle_connection(stream);
     }
+}
+fn handle_connection(mut stream: TcpStream) {
+    let reader = BufReader::new(&mut stream);
+    let req: Vec<_> = reader
+        .lines()
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
+    println!("Request {:#?}", req);
 }
